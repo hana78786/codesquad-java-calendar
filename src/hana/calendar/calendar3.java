3984c8c1593f13dd26000d928dcfc847bb58df71
hana78786 <upup78786@gmail.com>
2022-04-05 20:49:43 +0900

    매소드를 이용한 월별 날짜 계산

--- src/hana/calendar/calendar3.java
+++ src/hana/calendar/calendar3.java
@@ -1,20 +1,29 @@
 package hana.calendar;
 
+
 import java.util.Scanner;
 
+
 public class calendar3 {
+	private static final int i = 0;
+	private final static int[] MAX_DAYS={31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	public static int arr(int i) {
+		return MAX_DAYS[i-1];
+	} 
+	
 	public static void main(String[] args) {
 		System.out.println("달을 입력하시면 그 달이 며칠까지 있는지 출력합니다.");
 		Scanner scanner = new Scanner(System.in);
-		int i = scanner.nextInt();
-		if (i < 1) {
+		calendar3 cal=new calendar3();
+		int i=scanner.nextInt();
+		if (i< 1) {
 			System.out.println("1에서 12값을 입력하세요");
 		} else if(i>12){
 			System.out.println("1에서 12값을 입력하세요");
 		}
 		else {
-			int[] arr = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-			System.out.println(i + "월은 " + arr[i] + "일까지 있습니다");
+			
+			System.out.printf("%d월%d일까지 있습니다.\n",i,MAX_DAYS[i-1]);
 			scanner.close();
 		}
 	}
