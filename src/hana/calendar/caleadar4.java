6376f4c93129bfed49732452c9a61e48e40dddd4
hana78786 <upup78786@gmail.com>
2022-04-05 23:38:11 +0900

    입력오류문구 추가

--- src/hana/calendar/caleadar4.java
+++ src/hana/calendar/caleadar4.java
@@ -4,21 +4,26 @@ import java.util.Scanner;
 
 public class caleadar4 {
 public static void main(String[]args) {
-	System.out.println("몇개의 달을 확인할건가요?");
+	System.out.println("확인하고자 하는 갯수를 입력하시오");
+	int[] arr= {31,28,31,30,31,30,31,31,30,31,30,31};
 	Scanner s1=new Scanner(System.in);
-	int n=s1.nextInt();
-	System.out.println("몇일까지 있는지 궁금한 달을 입력하세요");
-	for(int a=1;a<n+1;a++) {
-	int[] arr={31,28,31,30,31,30,31,31,30,31,30,31};
-	Scanner scanner=new Scanner(System.in);
-	int i=scanner.nextInt();
-	System.out.printf("%d월은%d일까지 있습니다\n",i,arr[i-1]);
-}
-
-
-s1.close();
-	
-}
+	int i=s1.nextInt();
+	System.out.println("몇월이 며칠까지 있는지 궁금한가요?");
+	for(int a=1;a<i+1;a++) {
+		Scanner s2=new Scanner(System.in);
+		int b=s2.nextInt();
+		if(b<1) {System.out.println("잘못입력하셨습니다");}
+		else if(b>12){System.out.println("잘못입력하셨습니다");}
+		else {System.out.printf("%d월은 %d일까지 있습니다\n",b,arr[b-1]);}
+		
+		
 	}
+	s1.close();
 
+	System.out.println("프로그램을 종료합니다");
+}
+
+}
+
+	
 
