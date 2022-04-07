c9c76b3e62bb1ab207588e8eb5510eeb45aea6f0
hana78786 <upup78786@gmail.com>
2022-04-07 22:39:27 +0900

    윤련의 구현

--- src/hana/calendar/Prompt.java
+++ src/hana/calendar/Prompt.java
@@ -3,40 +3,32 @@ package hana.calendar;
 import java.util.Scanner;
 
 public class Prompt {
-	private final static String PROMPT = "> ";
-
-	public void runPrompt() {
-		Scanner s1 = new Scanner(System.in);
-		caleadar_sample cal1 = new caleadar_sample();
-		int i=1;
-		while (true) {
-			System.out.println("알고싶은 달의 수를 쓰세요");
-			System.out.print(PROMPT);
-			i = s1.nextInt();
-			int[] arr = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
-			if (i == -1) {
-				System.out.println("프로그램을 종료합니다");
-				break;
-			}
-			if (i < 1) {
-				System.out.println("잘못입력하셨습니다");
-				continue;
-			}
-			if (i > 12) {
-				System.out.println("잘못입력하셨습니다");
-				continue;
-			}
-			cal1.printCalendar(2017, i);
-		}
-		//System.out.printf("%d월은 %d일까지 입니다\n", i,i - 1);
-
+private final static String PROMPT=">";
+public void runPrompt() {
+	Scanner s1=new Scanner(System.in);
+	int mon=1;
+	int year=2017;
+	caleadar_sample cs=new caleadar_sample();
+	while(true) {
+		System.out.println("년도를입력하세요");
+		System.out.print("year> ");
+		year=s1.nextInt();
+		if(year==-1) {System.out.println("프로그램을 종료합니다");
+		break;}
+		System.out.println("달을 입력하세요");
+		System.out.print("month> ");
+		mon=s1.nextInt();
+		if(mon==-1) {System.out.println("프로그램을 종료합니다");
+		break;}
+		if(mon<1||mon>12) {System.out.println("잘못된입력입니다");
+		continue;}
+		cs.printcalendar(year, mon);
+		
+	
 	}
-
-	public static void main(String[] args) {
-		Prompt p = new Prompt();
-		p.runPrompt();
-		// 두 함수에대한 설명이 없다 왜 들어가는지만이라도 알려주면 좋을텐데
-	}
-
+}
+public static void main(String[]args) {
+	Prompt p=new Prompt();
+	p.runPrompt();
+}
 }
--- src/hana/calendar/caleadar_sample.java
+++ src/hana/calendar/caleadar_sample.java
@@ -1,31 +1,36 @@
 package hana.calendar;
 
-import java.util.Scanner;
+
 
 public class caleadar_sample {
-	private final static int[] MAX_DAYS={31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private final static int[] arr= {31,28,31,30,31,30,31,31,30,31,30,31};
+	private final static int[] larr= {31,29,31,30,31,30,31,31,30,31,30,31};
 	
-	public int arr(int i) {
-		return MAX_DAYS[i-1];
+	public boolean isLeepYear(int year) {
+		if(year %4 ==0 && (year%100 != 0||year%400==0))
+		return true;
+		else
+	return false;
 	}
 	
+	public int arr(int year,int mon) {
+		if (isLeepYear(year)) {
+			return larr[mon-1];
+		}else {
+		return arr[mon-1];
+	}}
 
-	public void printCalendar(int year,int i) {	
-		System.out.printf("   <<%4d   %3d>>\n",year,i);
-		System.out.println(" 일  월  화 수 목  금  토");
-		System.out.println("--------------------------");
-		int maxday = arr(i);
-		for(int a=1;a<=maxday;a++) {
-			System.out.printf("%3d",a);
-			if(a%7==0)
-				System.out.println();
-		}
-		System.out.println();
-//		System.out.println("1   2   3   4   5   6   7");
-//		System.out.println("8   9   10  11  12  13  14");
-//		System.out.println("15  16  17  18  19  20  21");
-//		System.out.println("22  23  24  25  26  27  28");
-		}
 	
+public void printcalendar(int year,int mon) {
+System.out.printf("   <<%4d %3d>>\n",year,mon);
+System.out.println(" sn mon tu we th fr sa");
+System.out.println("---------------------");
 
-	public static void main(String[]args){}}
+
+int md=arr(year,mon);
+for(int a=1;a<=md;a++) {
+	System.out.printf("%3d",a);
+	if(a%7==0) {
+		System.out.println();}	
+}System.out.println();}
+public static void main(String[]args) {}}
