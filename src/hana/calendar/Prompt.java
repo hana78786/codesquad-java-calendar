63852e2bfb3b2834802853448e84a5709b950e46
hana78786 <upup78786@gmail.com>
2022-04-08 23:49:41 +0900

    달력출력 테스트 중

--- src/hana/calendar/Prompt.java
+++ src/hana/calendar/Prompt.java
@@ -8,6 +8,7 @@ public void runPrompt() {
 	Scanner s1=new Scanner(System.in);
 	int mon=1;
 	int year=2017;
+	String b="su";
 	caleadar_sample cs=new caleadar_sample();
 	while(true) {
 		System.out.println("년도를입력하세요");
@@ -18,11 +19,14 @@ public void runPrompt() {
 		System.out.println("달을 입력하세요");
 		System.out.print("month> ");
 		mon=s1.nextInt();
+		System.out.println("시작 요일을 입력하세요 su mo tu...");
+		System.out.print("> ");
+		b=s1.next();
 		if(mon==-1) {System.out.println("프로그램을 종료합니다");
 		break;}
 		if(mon<1||mon>12) {System.out.println("잘못된입력입니다");
 		continue;}
-		cs.printcalendar(year, mon);
+		cs.printcalendar(year, mon, b);
 		
 	
 	}
--- src/hana/calendar/caleadar_sample.java
+++ src/hana/calendar/caleadar_sample.java
@@ -1,6 +1,6 @@
 package hana.calendar;
 
-
+import java.util.Scanner;
 
 public class caleadar_sample {
 	private final static int[] arr= {31,28,31,30,31,30,31,31,30,31,30,31};
@@ -21,16 +21,46 @@ public class caleadar_sample {
 	}}
 
 	
-public void printcalendar(int year,int mon) {
+public void printcalendar(int year,int mon, String b) {
 System.out.printf("   <<%4d %3d>>\n",year,mon);
-System.out.println(" sn mon tu we th fr sa");
+System.out.println(" sn mo tu we th fr sa");
 System.out.println("---------------------");
-
-
 int md=arr(year,mon);
+
 for(int a=1;a<=md;a++) {
+	if(b=="su") {
 	System.out.printf("%3d",a);
 	if(a%7==0) {
-		System.out.println();}	
+		System.out.println();}}
+	if(b=="mo") {
+		System.out.printf("%3d"," ");
+		System.out.printf("%3d",a);
+		if(a%7==6) {
+			System.out.println();}}
+	if(b=="tu") {
+		System.out.printf("%6d"," ");
+	System.out.printf("%3d",a);
+	if(a%7==5) {
+		System.out.println();}}
+	if(b=="we") {
+		System.out.printf("%9d"," ");
+	System.out.printf("%3d",a);
+	if(a%7==4) {
+		System.out.println();}}
+	if(b=="th") {
+		System.out.printf("%12d"," ");
+	System.out.printf("%3d",a);
+	if(a%7==3) {
+		System.out.println();}}
+	if(b=="fr") {
+		System.out.printf("%15d"," ");
+	System.out.printf("%3d",a);
+	if(a%7==2) {
+		System.out.println();}}
+	if(b=="sa") {
+		System.out.printf("%18d"," ");
+	System.out.printf("%3d",a);
+	if(a%7==1) {
+		System.out.println();}}
 }System.out.println();}
 public static void main(String[]args) {}}
