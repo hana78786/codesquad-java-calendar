35edec71c844a8b8fcee40d4da55edfce7b19a6c
hana78786 <upup78786@gmail.com>
2022-04-09 01:30:00 +0900

    요일입력구현

--- src/hana/calendar/Prompt.java
+++ src/hana/calendar/Prompt.java
@@ -3,11 +3,24 @@ package hana.calendar;
 import java.util.Scanner;
 
 public class Prompt {
+	public int pd(String week) {
+		if(week.equals("su"))
+		return 0;
+		else if(week.equals("mo")) return 1;
+		else if(week.equals("tu")) return 2;
+		else if(week.equals("we")) return 3;
+		else if(week.equals("th")) return 4;
+		else if(week.equals("fr")) return 5;
+		else if(week.equals("sa")) return 6;
+		else return 0;
+	
+	}
 private final static String PROMPT=">";
 public void runPrompt() {
 	Scanner s1=new Scanner(System.in);
 	int mon=1;
 	int year=2017;
+	int week=0;
 	String b="su";
 	caleadar_sample cs=new caleadar_sample();
 	while(true) {
@@ -19,14 +32,14 @@ public void runPrompt() {
 		System.out.println("달을 입력하세요");
 		System.out.print("month> ");
 		mon=s1.nextInt();
-		System.out.println("시작 요일을 입력하세요 su mo tu...");
-		System.out.print("> ");
-		b=s1.next();
 		if(mon==-1) {System.out.println("프로그램을 종료합니다");
 		break;}
 		if(mon<1||mon>12) {System.out.println("잘못된입력입니다");
 		continue;}
-		cs.printcalendar(year, mon, b);
+		System.out.println("요일입력 su mo..");
+		String str_week = s1.next();
+		week=pd(str_week);
+		cs.printcalendar(year, mon, week);
 		
 	
 	}
--- src/hana/calendar/caleadar_sample.java
+++ src/hana/calendar/caleadar_sample.java
@@ -2,9 +2,11 @@ package hana.calendar;
 
 import java.util.Scanner;
 
+
 public class caleadar_sample {
 	private final static int[] arr= {31,28,31,30,31,30,31,31,30,31,30,31};
 	private final static int[] larr= {31,29,31,30,31,30,31,31,30,31,30,31};
+	private String b;
 	
 	public boolean isLeepYear(int year) {
 		if(year %4 ==0 && (year%100 != 0||year%400==0))
@@ -21,46 +23,28 @@ public class caleadar_sample {
 	}}
 
 	
-public void printcalendar(int year,int mon, String b) {
-System.out.printf("   <<%4d %3d>>\n",year,mon);
-System.out.println(" sn mo tu we th fr sa");
+public void printcalendar(int year,int mon, int week) {
+	
+	System.out.printf("   <<%4d %3d>>\n",year,mon);
+System.out.println(" su mo tu we th fr sa");
 System.out.println("---------------------");
-int md=arr(year,mon);
 
-for(int a=1;a<=md;a++) {
-	if(b=="su") {
+for(int a=0;a<week;a++) {
+	System.out.print("   ");
+}
+int md=arr(year,mon);
+int count=7-week;
+int d= (count<7)?count:0;
+for(int a=1;a<=count;a++) {
+	System.out.printf("%3d",a);}
+		System.out.println();	
+
+count++;
+for(int a=count;a<=md;a++) {
 	System.out.printf("%3d",a);
-	if(a%7==0) {
-		System.out.println();}}
-	if(b=="mo") {
-		System.out.printf("%3d"," ");
-		System.out.printf("%3d",a);
-		if(a%7==6) {
-			System.out.println();}}
-	if(b=="tu") {
-		System.out.printf("%6d"," ");
-	System.out.printf("%3d",a);
-	if(a%7==5) {
-		System.out.println();}}
-	if(b=="we") {
-		System.out.printf("%9d"," ");
-	System.out.printf("%3d",a);
-	if(a%7==4) {
-		System.out.println();}}
-	if(b=="th") {
-		System.out.printf("%12d"," ");
-	System.out.printf("%3d",a);
-	if(a%7==3) {
-		System.out.println();}}
-	if(b=="fr") {
-		System.out.printf("%15d"," ");
-	System.out.printf("%3d",a);
-	if(a%7==2) {
-		System.out.println();}}
-	if(b=="sa") {
-		System.out.printf("%18d"," ");
-	System.out.printf("%3d",a);
-	if(a%7==1) {
-		System.out.println();}}
-}System.out.println();}
+	if(a%7==d) {
+		System.out.println();}	
+}
+
+System.out.println();}
 public static void main(String[]args) {}} 
