3d8188359d75143cf5e4eca455d9c4f679ab716e
hana78786 <upup78786@gmail.com>
2022-04-11 18:15:22 +0900

    count를 이용한 달력의 구현

--- src/hana/calendar/Prompt.java
+++ src/hana/calendar/Prompt.java
@@ -3,51 +3,62 @@ package hana.calendar;
 import java.util.Scanner;
 
 public class Prompt {
-	public int pd(String week) {
-		if(week.equals("su"))
-		return 0;
-		else if(week.equals("mo")) return 1;
-		else if(week.equals("tu")) return 2;
-		else if(week.equals("we")) return 3;
-		else if(week.equals("th")) return 4;
-		else if(week.equals("fr")) return 5;
-		else if(week.equals("sa")) return 6;
-		else if(week.equals("-1")) return -1;
-		else return 7;
-	
+//	public int pd(String week) {
+//		if (week.equals("su"))
+//			return 0;
+//		else if (week.equals("mo"))
+//			return 1;
+//		else if (week.equals("tu"))
+//			return 2;
+//		else if (week.equals("we"))
+//			return 3;
+//		else if (week.equals("th"))
+//			return 4;
+//		else if (week.equals("fr"))
+//			return 5;
+//		else if (week.equals("sa"))
+//			return 6;
+//		else if (week.equals("-1"))
+//			return -1;
+//		else
+//			return 7;
+//
+//	}
+
+	public void runPrompt() {
+		Scanner s1 = new Scanner(System.in);
+		int mon = 1;
+		int year = 2017;
+		int week = 0;
+		caleadar_sample cs = new caleadar_sample();
+		while (true) {
+			System.out.println("년도를입력하세요");
+			System.out.print("year> ");
+			year = s1.nextInt();
+			if (year == -1) {
+				System.out.println("프로그램을 종료합니다");
+				break;
+			}
+			System.out.println("달을 입력하세요");
+			System.out.print("month> ");
+			mon = s1.nextInt();
+			if (mon == -1) {
+				System.out.println("프로그램을 종료합니다");
+				break;
+			}
+			if (mon < 1 || mon > 12) {
+				System.out.println("잘못된입력입니다");
+				continue;
+			}
+
+			
+			cs.printcalendar(year, mon);
+
+		}
 	}
 
-public void runPrompt() {
-	Scanner s1=new Scanner(System.in);
-	int mon=1;
-	int year=2017;
-	int week=0;
-	caleadar_sample cs=new caleadar_sample();
-	while(true) {
-		System.out.println("년도를입력하세요");
-		System.out.print("year> ");
-		year=s1.nextInt();
-		if(year==-1) {System.out.println("프로그램을 종료합니다");
-		break;}
-		System.out.println("달을 입력하세요");
-		System.out.print("month> ");
-		mon=s1.nextInt();
-		if(mon==-1) {System.out.println("프로그램을 종료합니다");
-		break;}
-		if(mon<1||mon>12) {System.out.println("잘못된입력입니다");
-		continue;}
-		System.out.println("요일입력 su mo..");
-		System.out.print("> ");
-		String str_week = s1.next();
-		week=pd(str_week);
-		if(week==-1) {System.out.println("종료합니다"); break;}
-		cs.printcalendar(year, mon, week);
-		
-	
+	public static void main(String[] args) {
+		Prompt p = new Prompt();
+		p.runPrompt();
 	}
-}
-public static void main(String[]args) {
-	Prompt p=new Prompt();
-	p.runPrompt();
-} 
 }
--- src/hana/calendar/caleadar_sample.java
+++ src/hana/calendar/caleadar_sample.java
@@ -1,46 +1,81 @@
 package hana.calendar;
 
-
-
-
 public class caleadar_sample {
-	private final static int[] arr= {31,28,31,30,31,30,31,31,30,31,30,31};
-	private final static int[] larr= {31,29,31,30,31,30,31,31,30,31,30,31};
-	
-	
+	private final static int[] arr = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private final static int[] larr = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public boolean isLeepYear(int year) {
-		if(year %4 ==0 && (year%100 != 0||year%400==0))
-		return true;
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+			return true;
 		else
-	return false;
+			return false;
 	}
-	
-	public int arr(int year,int mon) {
+
+	public int arr(int year, int mon) {
 		if (isLeepYear(year)) {
-			return larr[mon-1];
-		}else {
-		return arr[mon-1];
-	}}
+			return larr[mon];
+		} else {
+			return arr[mon];
+		}
+	}
 
 	
-public void printcalendar(int year,int mon, int week) {
-	if(week==7) {System.out.println("잘못된입력입니다");}
+	public void printcalendar(int year, int mon) {
+		System.out.printf("   <<%4d %3d>>\n", year, mon);
+		System.out.println(" su mo tu we th fr sa");
+		System.out.println("---------------------");
 
-	else {
-	System.out.printf("   <<%4d %3d>>\n",year,mon);
-System.out.println(" su mo tu we th fr sa");
-System.out.println("---------------------");
+		
+		int weekday = getweekday(year, mon, 1);
+		// System.out.print(week);
+		
+		for (int a = 0; a < weekday; a++) {
+			System.out.print("   ");
+		}
+	
+		int md = arr(year, mon);
+		int count = 7 - weekday;
+		int delim = (count < 7) ? count : 0;
+		
+		for (int a = 1; a <= count; a++) {
+			System.out.printf("%3d", a);
+		}
+				System.out.println();
+				
+				count++;
+				for(int a = count; a <= md; a++) {
+					System.out.printf("%3d",a);
+					if (a % 7 == delim) 
+						System.out.println();
+			}
+			 System.out.println();
 
-for(int a=0;a<week;a++) {
-	System.out.print("   ");
+
+				}
+		
+	
+
+	private int getweekday(int year, int mon, int day) {
+		int syear = 1970;
+	
+		final int STAND_WEEKDAY = 3;// 1970 jan 1st thursday
+
+		int count = 0;
+		for (int a = syear; a < year; a++) {
+			int delta = isLeepYear(a) ? 366 : 365;
+			count += delta;
+		}
+		for (int a = 1; a < mon; a++) {
+			int delta = arr(year, a);
+			count += delta;
+		}
+
+		count += day;
+		int weekday = (count + STAND_WEEKDAY) % 7;
+		return weekday;
+
+	}
+
+	public static void main(String[] args) {
+	}
 }
-int md=arr(year,mon);
-
-for(int a=1;a<=md;a++) {
-	System.out.printf("%3d",a);
-	if((a+week)%7==0) {
-		System.out.println();}	
-}}
-
-System.out.println();}
-public static void main(String[]args) {}} 
