40c88fb5cb6a8f11c45e365e74b76781917d8e11
hana78786 <upup78786@gmail.com>
2022-04-13 22:53:34 +0900

    일정등록용 HashMap

--- src/hana/calendar/hashpmap.java
+++ src/hana/calendar/hashpmap.java
@@ -15,21 +15,11 @@ String a=scanner.next();
 System.out.println("일정을 입력하세요");
 String b=scanner.next();
 hm.put(a, b);
-hm.put("2", "world2");
-hm.put("3", "hello3");
-hm.put("4", "world4");
-hm.put("2", "WorldWorld2");
-System.out.print("size:"+hm.size());
+
+System.out.print("일정수:"+hm.size());
 System.out.println();
 
 for(Map.Entry<String,String>e:hm.entrySet())
-	System.out.println("Key:"+e.getKey()+"Value:"+e.getValue());
+	System.out.println("날짜:"+e.getKey()+" 일정:"+e.getValue());
 
-
-
-hm.remove("3");
-System.out.println(hm);
-
-hm.clear();
-System.out.println(hm);
 }}
