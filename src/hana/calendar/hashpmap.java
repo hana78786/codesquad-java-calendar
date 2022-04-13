5955492de8b93e3ad714a77928cfac3d22ef0722
hana78786 <upup78786@gmail.com>
2022-04-13 22:51:01 +0900

    일정입력테스트

--- src/hana/calendar/hashpmap.java
+++ src/hana/calendar/hashpmap.java
@@ -2,11 +2,19 @@ package hana.calendar;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class hashpmap {
 public static void main(String[] args) {
+	
+	
 HashMap<String,String>hm=new HashMap<String,String>();
-hm.put("1", "hello1");
+System.out.println("날짜를 입력하세요");
+Scanner scanner=new Scanner(System.in);
+String a=scanner.next();
+System.out.println("일정을 입력하세요");
+String b=scanner.next();
+hm.put(a, b);
 hm.put("2", "world2");
 hm.put("3", "hello3");
 hm.put("4", "world4");
