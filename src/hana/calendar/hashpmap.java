24c5c10e6af14f98e49cb4ae4f263b416b4a29ab
hana78786 <upup78786@gmail.com>
2022-04-13 22:32:41 +0900

    hashpmap의 사용

--- src/hana/calendar/hashpmap.java
+++ src/hana/calendar/hashpmap.java
@@ -1,6 +1,7 @@
 package hana.calendar;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class hashpmap {
 public static void main(String[] args) {
@@ -10,7 +11,13 @@ hm.put("2", "world2");
 hm.put("3", "hello3");
 hm.put("4", "world4");
 hm.put("2", "WorldWorld2");
-System.out.print(hm);
+System.out.print("size:"+hm.size());
+System.out.println();
+
+for(Map.Entry<String,String>e:hm.entrySet())
+	System.out.println("Key:"+e.getKey()+"Value:"+e.getValue());
+
+
 
 hm.remove("3");
 System.out.println(hm);
