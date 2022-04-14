7cabffcce37f3a618e0e5ad1e19084cf375f8afe
hana78786 <upup78786@gmail.com>
2022-04-14 22:59:17 +0900

    일정들록1

--- src/hana/calendar/Prompt.java
+++ src/hana/calendar/Prompt.java
@@ -1,47 +1,55 @@
 package hana.calendar;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Prompt {
-//	public int pd(String week) {
-//		if (week.equals("su"))
-//			return 0;
-//		else if (week.equals("mo"))
-//			return 1;
-//		else if (week.equals("tu"))
-//			return 2;
-//		else if (week.equals("we"))
-//			return 3;
-//		else if (week.equals("th"))
-//			return 4;
-//		else if (week.equals("fr"))
-//			return 5;
-//		else if (week.equals("sa"))
-//			return 6;
-//		else if (week.equals("-1"))
-//			return -1;
-//		else
-//			return 7;
-//
-//	}
-
+	
+	public void printmenu(){
+		System.out.println("+---------------+");
+		System.out.println("|1.일정 등록");
+		System.out.println("|2.일정 검색");
+		System.out.println("|3.달력 보기");
+		System.out.println("|h.도움말 q.종료");
+		System.out.println("+---------------+");
+//		System.out.println("명령 (1,2,3,h,q");
+	}
 	public void runPrompt() {
-		Scanner s1 = new Scanner(System.in);
-		int mon = 1;
-		int year = 2017;
-		int week = 0;
+		printmenu();
+		Scanner scanner = new Scanner(System.in);
+		
 		caleadar_sample cs = new caleadar_sample();
+while (true) {
+	System.out.println();
+	System.out.println("명령 (1,2,3,h,q)");
+		String cmd=scanner.next();
+		if(cmd.equals("1")) cmdRegister();
+		else if (cmd.equals("2"))  cmdSerch();
+		else if (cmd.equals("3")) cmdCal1(scanner, cs);
+		else if (cmd.equals("h")) printmenu();
+		else if (cmd.equals("q")) break;
+	}
+	System.out.println("thank you");
+	scanner.close();
+	}
+	
+		private void cmdCal1(Scanner s, caleadar_sample cs) {
+			
+			int mon = 1;
+			int year = 2017;
+			int week = 0;
 		while (true) {
 			System.out.println("년도를입력하세요");
 			System.out.print("year> ");
-			year = s1.nextInt();
+			year = s.nextInt();
 			if (year == -1) {
 				System.out.println("프로그램을 종료합니다");
 				break;
 			}
 			System.out.println("달을 입력하세요");
 			System.out.print("month> ");
-			mon = s1.nextInt();
+			mon = s.nextInt();
 			if (mon == -1) {
 				System.out.println("프로그램을 종료합니다");
 				break;
@@ -53,10 +61,26 @@ public class Prompt {
 
 			
 			cs.printcalendar(year, mon);
+			return;
 
 		}
+		
+		
+		
 	}
 
+	private void cmdCal() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void cmdSerch() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void cmdRegister() {
+		// TODO Auto-generated method stub
+		
+	}
 	public static void main(String[] args) {
 		Prompt p = new Prompt();
 		p.runPrompt();
